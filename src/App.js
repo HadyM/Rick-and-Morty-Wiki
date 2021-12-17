f994e4590ea494543e308e41577c231c156ca6ec
Hady Mohamed <hadymohamed@pursuit.org>
2021-12-16 21:57:52 -0500

    Updated Card.js to render characters and created Card.module.scss file styling badges

--- src/App.js
+++ src/App.js
@@ -11,19 +11,33 @@ import Navbar from "./components/Navbar/Navbar.js";
 import "./App.css";
 
 function App() {
-  return <div className="App">
-    <h1 className="text-center mb-3">Characters</h1>
-    <div className="container">
-      <div className="row">
-        Filter component will be placed here
-        <div className="col-lg-8 col-12">
-          <div className="row">
-            Card component will be placed here
+  let api = `https://rickandmortyapi.com/api/character/?page=1`;
+
+  let [fetchedData, updateFetchedData] = useState([]);
+  let { info, results } = fetchedData;
+
+  useEffect(() => {
+    (async function () {
+      let data = await fetch(api).then((res) => res.json());
+      updateFetchedData(data);
+    })();
+  }, [api]);
+
+  return (
+    <div className="App">
+      <h1 className="text-center mb-3">Characters</h1>
+      <div className="container">
+        <div className="row">
+          Filter component will be placed here
+          <div className="col-lg-8 col-12">
+            <div className="row">
+              <Card results={results} />
+            </div>
           </div>
         </div>
       </div>
     </div>
-  </div>;
+  );
 }
 
 export default App;
