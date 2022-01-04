6d815a341c3f5d22dd7d4711e0a3a78c5c69980b
Hady Mohamed <hadymohamed@pursuit.org>
2022-01-03 19:01:25 -0500

    finished filter bar with 3 categories and a bunch of sub categories, some styling

--- src/App.js
+++ src/App.js
@@ -14,9 +14,12 @@ function App() {
   let [pageNumber, updatePageNumber] = useState(1);
   let [search, setSearch] = useState("");
   let [fetchedData, updateFetchedData] = useState([]);
+  let [status, updateStatus] = useState("");
+  let [gender, updateGender] = useState("");
+  let [species, updateSpecies] = useState("");
   let { info, results } = fetchedData;
 
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
 
   useEffect(() => {
     (async function () {
@@ -31,7 +34,14 @@ function App() {
       <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
       <div className="container">
         <div className="row">
-          Filter component will be placed here!!!
+          <Filter
+            pageNumber={pageNumber}
+            status={status}
+            updateStatus={updateStatus}
+            updateGender={updateGender}
+            updateSpecies={updateSpecies}
+            updatePageNumber={updatePageNumber}
+          />
           <div className="col-lg-8 col-12">
             <div className="row">
               <Card results={results} />
